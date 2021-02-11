c59472fa59b5153708aaf9680f7187e9f714ef2b
Saber Alipour <alipouor@gmail.com>
2021-02-11 12:55:35 +0330

    Add Overview component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import 'stylesheets/main.scss';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ['./pages/**/*.{ts,tsx,js,jsx}', './src/components/**/*.{ts,tsx,js,jsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
