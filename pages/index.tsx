5a1790c8d72c8c515eb699f98f7e94b208dccd7e
Saber Alipour <alipouor@gmail.com>
2021-02-19 00:11:15 +0330

    Fix src

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home({ data }: any): ReactElement {
             <meta property='og:site_name' content='Covid-19 Alert' />
             <meta
                property='og:image'
-               content={'static/assets/images/logo-image.jpg'}
+               content={'/static/assets/images/logo-image.jpg'}
             />
             <meta property='og:image:width' content='250' />
             <meta property='og:image:height' content='250' />
