d4f5c901ba9e5bdbefa068757b7adfbb5133746c
Saber Alipour <alipouor@gmail.com>
2021-02-18 22:31:06 +0330

    Fix tailswind try #02

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
    purge: [
       './pages/**/*.{ts,tsx,js,jsx}',
-      './lib/components/**/*.{ts,tsx,js,jsx}',
+      './components/**/*.{ts,tsx,js,jsx}',
    ],
    darkMode: false, // or 'media' or 'class'
    theme: {
